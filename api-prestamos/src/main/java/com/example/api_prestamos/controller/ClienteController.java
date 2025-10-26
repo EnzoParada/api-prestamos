0969d725e9be98b8b6ceafd78709c3765f695594
EnzoParada <paradaenzo26@outlook.com>
2025-10-26 17:07:39 -0300

    detalles clienteService

--- api-prestamos/src/main/java/com/example/api_prestamos/controller/ClienteController.java
+++ api-prestamos/src/main/java/com/example/api_prestamos/controller/ClienteController.java
@@ -1,10 +1,8 @@
 package com.example.api_prestamos.controller;
 
 import com.example.api_prestamos.model.Cliente;
-import com.example.api_prestamos.repository.ClienteRepository;
 import com.example.api_prestamos.service.ClienteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +17,7 @@ public class ClienteController {
 
     @PostMapping
     public ResponseEntity<Cliente> crearCliete(@RequestBody Cliente cliente){
-        return new ResponseEntity<>(clienteService.CrearCliente(cliente), HttpStatus.CREATED);
+        return new ResponseEntity<>(clienteService.crearCliente(cliente), HttpStatus.CREATED);
     }
 
     @GetMapping
@@ -39,14 +37,14 @@ public class ClienteController {
             @PathVariable Long id,
             @RequestBody Cliente clienteDetalles){
 
-        return clienteService.actualizaCliente(id, clienteDetalles)
+        return clienteService.actualizarCliente(id, clienteDetalles)
                 .map(clienteActualizado -> new ResponseEntity<>(clienteActualizado, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> bajaCliente(@PathVariable Long id){
-        clienteService.bajaCliente(id);
+        clienteService.eliminarCliente(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- api-prestamos/src/main/java/com/example/api_prestamos/service/ClienteService.java
+++ api-prestamos/src/main/java/com/example/api_prestamos/service/ClienteService.java
@@ -5,47 +5,54 @@ import com.example.api_prestamos.model.Prestamo;
 import com.example.api_prestamos.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ClienteService {
+
     private final ClienteRepository clienteRepository;
 
-    public Cliente CrearCliente(Cliente cliente){
-        if(cliente.getPrestamos() != null && !cliente.getPrestamos().isEmpty()){
-            for (Prestamo prestamo : cliente.getPrestamos()){
+    public Cliente crearCliente(Cliente cliente) {
+
+        if (cliente.getPrestamos() != null && !cliente.getPrestamos().isEmpty()) {
+
+            for (Prestamo prestamo : cliente.getPrestamos()) {
+
                 prestamo.setCliente(cliente);
             }
         }
         return clienteRepository.save(cliente);
     }
-    public List<Cliente> obtenerTodosLosClientes(){
+
+    public List<Cliente> obtenerTodosLosClientes() {
         return clienteRepository.findAllByBajaFalse();
     }
-    public Optional<Cliente> obtenerClientePorId(Long id){
+
+    public Optional<Cliente> obtenerClientePorId(Long id) {
         return clienteRepository.findByIdAndBajaFalse(id);
     }
-    public Optional<Cliente> actualizaCliente(Long id, Cliente clienteDetalles){
-        return clienteRepository.findById(id)
-                .map(clienteExistente ->{
+
+    public Optional<Cliente> actualizarCliente(Long id, Cliente clienteDetalles) {
+        return clienteRepository.findByIdAndBajaFalse(id)
+                .map(clienteExistente -> {
                     clienteExistente.setNombre(clienteDetalles.getNombre());
                     clienteExistente.setApellido(clienteDetalles.getApellido());
                     clienteExistente.setDni(clienteDetalles.getDni());
                     return clienteRepository.save(clienteExistente);
                 });
     }
-    public void bajaCliente(Long id){
+
+    public void eliminarCliente(Long id) {
         Optional<Cliente> clienteOpt = clienteRepository.findById(id);
 
-        if (clienteOpt.isPresent()){
+        if (clienteOpt.isPresent()) {
             Cliente cliente = clienteOpt.get();
             cliente.setBaja(true);
             clienteRepository.save(cliente);
-        }else{
-            throw new RuntimeException("Cliente no encontrado");
+        } else {
+            throw new RuntimeException("Cliente no encontrado con id: " + id);
         }
     }
 }
